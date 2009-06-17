fc1d6c53cf1f0c1d0cf3aa24e21ad6882821ddf6
Ivan <ivan@Ivans-BlackBook.local>
2009-06-17 09:40:05 -0500

    Added flagfriend module

---
+++ sites/all/modules/flag_friend/flag_friend.popups.js
@@ -0,0 +1,21 @@
+/**
+ * @file flag_friend.popups.js
+ * Popups submit handler.
+ */
+(function ($) {
+  
+    Drupal.flagFriendPopupsAfterSubmit = function(data, options, element) {
+      var $element = $(element);
+      var msg = '';
+      if ($element.hasClass('unflag-action')) {
+        msg = 'Friend request canceled.';
+      }
+      if ($element.hasClass('flag-action')) {
+        msg = 'Friend requested.';
+      }
+      Popups.message('Flag friend', msg);
+      location.reload();
+      return false;
+    };
+  
+})(jQuery);
