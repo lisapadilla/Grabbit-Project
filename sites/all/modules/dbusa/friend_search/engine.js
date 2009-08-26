188cf9f846923d0b94e8583b24ee19c35e046e35
Ivan <ivan@Ivans-BlackBook.gateway.2wire.net>
2009-08-26 14:55:49 -0500

    debug

---
+++ sites/all/modules/dbusa/friend_search/engine.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	
+	
+	$(".grabbit-friends").click(function(){
+	       $(".grabbit-container").slideToggle();
+	   });
+	$(".facebook-friends").click(function(){
+	       $(".facebook-container").slideToggle();
+	   });
+	$(".twitter-friends").click(function(){
+	       $(".twitter-container").slideToggle();
+	   });
+	
+	
+});
