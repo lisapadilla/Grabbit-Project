cd2570bff14755a0e5bdb45389bf4a55d02403cc
Ivan <ivan@Ivans-BlackBook.gateway.2wire.net>
2009-08-28 12:03:21 -0500

    changes

--- sites/all/modules/dbusa/grabbit_panels/engine.js
+++ sites/all/modules/dbusa/grabbit_panels/engine.js
@@ -11,11 +11,6 @@ $(document).ready(function() {
 	        trail: [ "...<a href='#' class='truncate_show'>more</a>", "<a href='#' class='truncate_hide'>less</a>" ]
 	   });
 	
-	$(".facebook-post .content-truncated").truncate( 120,{
-	        chars: /\s/,
-	        trail: [ "...<a href='#' class='truncate_show'>more</a>", "<a href='#' class='truncate_hide'>less</a>" ]
-	   });
-	
 	
 	
 });
