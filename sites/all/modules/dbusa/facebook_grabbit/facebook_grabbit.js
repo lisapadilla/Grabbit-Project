a8393a07b0736e371623d6b8685e5435a0f64e56
Ivan <ivan@ivans-blackbook.gateway.2wire.net>
2009-08-31 16:30:45 -0500

    javascript

--- sites/all/modules/dbusa/facebook_grabbit/facebook_grabbit.js
+++ sites/all/modules/dbusa/facebook_grabbit/facebook_grabbit.js
@@ -5,10 +5,10 @@ $(document).ready(function() {
 	var stories_per_slide_p1 = 4;
 	var current_panel_1 = $('#panel-0 .panel-wraper div:first-child');
 
-/*	$("#"+current_panel_1.attr('id')+" .content-truncated").truncate( 80,{
+	$("#"+current_panel_1.attr('id')+" .content-truncated").truncate( 80,{
 	        trail: [ "...<a href='#' class='truncate_show'>more</a>", "<a href='#' class='truncate_hide'>less</a>" ]
 	});
-*/	current_panel_1.show();
+	current_panel_1.show();
 	
 	for (var i = 1; i< stories_at_first_p1; i++){
 		current_panel_1 = current_panel_1.next();
@@ -45,7 +45,7 @@ $(document).ready(function() {
 	var stories_at_first_p2 = 4;
 	var stories_per_slide_p2 = 4;
 	var current_panel_2 = $('#panel-1 .panel-wraper div:first-child');
-	alert($("#"+current_panel_2.attr('id')+" .content-truncated").html());
+
 /*	$("#"+current_panel_2.attr('id')+" .content-truncated").truncate( 80,{
 	        trail: [ "...<a href='#' class='truncate_show'>more</a>", "<a href='#' class='truncate_hide'>less</a>" ]
 	});
