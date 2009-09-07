f018828ce8bccd4bb40bfc55daa8eb66dc9cd334
Ivan <ivan@ivans-blackbook.gateway.2wire.net>
2009-09-07 15:22:09 -0500

    inviter

--- sites/all/modules/dcl_importer/importers/openinviter_engine/OpenInviter/config.php
+++ sites/all/modules/dcl_importer/importers/openinviter_engine/OpenInviter/config.php
@@ -5,7 +5,7 @@
 		"cookie_path"=>'/tmp',
 		"message_body"=>"You are invited to grabbit.net", // grabbit.net is the website on your account. If wrong, please update your account at OpenInviter.com
 		"message_subject"=>" is inviting you to grabbit.net", // grabbit.net is the website on your account. If wrong, please update your account at OpenInviter.com
-		"transport"=>"curl", //Replace "curl" with "wget" if you would like to use wget instead
+		"transport"=>"wget", //Replace "curl" with "wget" if you would like to use wget instead
 		"local_debug"=>"on_error", //Available options: on_error => log only requests containing errors; always => log all requests; false => don`t log anything
 		"remote_debug"=>FALSE, //When set to TRUE OpenInviter sends debug information to our servers. Set it to FALSE to disable this feature
 		"hosted"=>FALSE, //When set to TRUE OpenInviter uses the OpenInviter Hosted Solution servers to import the contacts.
