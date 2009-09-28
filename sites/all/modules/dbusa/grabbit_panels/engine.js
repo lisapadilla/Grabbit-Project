ad5d390fb255c224fc6a3fc0488b664146067bb5
Ivan <ivan@ivans-blackbook.gateway.2wire.net>
2009-09-28 15:50:33 -0500

    druaj behaviors[A

--- sites/all/modules/dbusa/grabbit_panels/engine.js
+++ sites/all/modules/dbusa/grabbit_panels/engine.js
@@ -1,6 +1,7 @@
 Drupal.behaviors.grabbitPanelsBehavior = function (context) {
   $('.panel-closer').click(function(element){
 	 var panel=$(this).attr('panelid');
+	alert(panel);
 	 $.get(Drupal.settings.basePath+"panels/delete",{panel:panel},function(data){
 				if (data){
 					$('#panel-'+panel).slideToggle('slow');
