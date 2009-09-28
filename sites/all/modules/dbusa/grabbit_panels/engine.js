64cbc8ec9e0059170fcf6080ff1dc74dc687d5f8
Ivan <ivan@ivans-blackbook.gateway.2wire.net>
2009-09-28 15:48:03 -0500

    druaj behaviors[A

--- sites/all/modules/dbusa/grabbit_panels/engine.js
+++ sites/all/modules/dbusa/grabbit_panels/engine.js
@@ -1,3 +1,13 @@
 Drupal.behaviors.grabbitPanelsBehavior = function (context) {
-  
+  $('.panel-closer').click(function(element){
+	 var panel=$(this).attr('panelid');
+	 $.get(Drupal.settings.basePath+"panels/delete",{panel:panel},function(data){
+				if (data){
+					$('#panel-'+panel).slideToggle('slow');
+				}else{
+					alert("Oops! The server is not responding, please try again");
+				}
+	    });
+	
+  });
 };
