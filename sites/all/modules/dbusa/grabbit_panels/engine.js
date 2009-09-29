ada340fa17c033dbfa4dfd132876082f07c05aa6
Ivan <ivan@ivans-blackbook.gateway.2wire.net>
2009-09-29 16:25:49 -0500

    correcciones

--- sites/all/modules/dbusa/grabbit_panels/engine.js
+++ sites/all/modules/dbusa/grabbit_panels/engine.js
@@ -1,14 +1,34 @@
-Drupal.behaviors.grabbitPanelsBehavior = function (context) {
-  $('.panel-closer').click(function(element){
-	 var panel=$(this).attr('panelid');
-	 $.get(Drupal.settings.basePath+"panels/delete",{panel:panel},function(data){
-				if (data){
-					alert(data);
-					$('#panel-'+panel).slideToggle('slow');
+Drupal.behaviors.grabbitPanelsAdjust = function (context) {
+
+  $('.panel-closer').click(function(elem){
+	var panel=$(this).attr('panelid');
+	
+	$.get(Drupal.settings.basePath+"panels/delete",{panel:panel},function(data){
+			if (data){
+			  	$('#panel-'+panel).remove();
+				$('.panels-pager').empty();
+				$('.user-panels').cycle('stop');
+				ct = $('.user-panels').children().size();
+				
+				if (ct>1){
+					$('.user-panels').cycle({
+					    fx:      'scrollHorz',
+					    next:    '.move-right',
+					    prev:    '.move-left',
+					    pager: '.panels-pager',
+					    timeout:  0,
+					    prevNextClick: fixtheheight
+					});	
 				}else{
-					alert("Oops! The server is not responding, please try again");
+					$('.user-panels div').show();
+					$('.panel-closer').hide();
 				}
-	    });
+				
+			}else{
+				alert("Oops! There was a connection problem. Try later later");
+			}
+    });
 	
   });
+
 };
