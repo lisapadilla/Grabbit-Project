e12e0ca035feff5092f5108769dfd6455f85b90e
Ivan <ivan@ivans-blackbook.gateway.2wire.net>
2009-10-02 12:44:11 -0500

    Feature added: You can now delete grabbit pannels (refresh cache firs)

--- sites/all/modules/dbusa/grabbit_panels/engine.js
+++ sites/all/modules/dbusa/grabbit_panels/engine.js
@@ -1,37 +1,38 @@
 Drupal.behaviors.grabbitPanelsAdjust = function (context) {
 
+//JQuery alerts to delete panels (with confirmation) - grabbit streams.
   $('.panel-closer').click(function(elem){
 	var panel=$(this).attr('panelid');
 	jConfirm("Do you really want to delete this panel? This action cannot be undone.", 'Delete Panel',function(confirm){
-	  if(confirm==false){
-	    return 0;	
-	  }
+	if(confirm==false){
+	  return 0;	// return empty to avoit panel deletion 
+	}
 	
-	    $.get(Drupal.settings.basePath+"panels/delete",{panel:panel},function(data){
-			if (data){
-				$('.move-left').click();
-			  	$('#panel-'+panel).remove();
-				$('.panels-pager').empty();
-				$('.user-panels').cycle('stop');
-				ct = $('.user-panels').children().size();
+	$.get(Drupal.settings.basePath+"panels/delete",{panel:panel},function(data){
+		if (data){
+		  $('.move-left').click();
+		  $('#panel-'+panel).remove();
+		  $('.panels-pager').empty();
+		  $('.user-panels').cycle('stop');
+		  ct = $('.user-panels').children().size();
 				
-				if (ct>1){
-					$('.user-panels').cycle({
-					    fx:      'scrollHorz',
-					    next:    '.move-right',
-					    prev:    '.move-left',
-					    pager: '.panels-pager',
-					    timeout:  0,
-					    prevNextClick: fixtheheight
-					});	
-				}else{
-					$('.user-panels div').show();
-					$('.panel-closer').hide();
-				}
-			}else{
-				alert("Oops! There was a connection problem. Try later later");
-			}
-        });
+		  if (ct>1){
+		    $('.user-panels').cycle({
+			  fx:      'scrollHorz',
+			  next:    '.move-right',
+		      prev:    '.move-left',
+			  pager: '.panels-pager',
+			  timeout:  0,
+		      prevNextClick: fixtheheight
+		    });	
+		  }else{
+			$('.user-panels div').show();
+			$('.panel-closer').hide();
+		  }
+		}else{
+		  alert("Oops! There was a connection problem. Try later later");
+		}
+     });
 	
     });
   });
