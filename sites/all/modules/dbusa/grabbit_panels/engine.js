3cc7b6eb59a97c91ffc82b65919583bc6aecd8a3
Ivan <ivan@ivans-blackbook.gateway.2wire.net>
2009-10-16 16:50:06 -0500

    Testing file uploads

--- sites/all/modules/dbusa/grabbit_panels/engine.js
+++ sites/all/modules/dbusa/grabbit_panels/engine.js
@@ -37,4 +37,38 @@ Drupal.behaviors.grabbitPanelsAdjust = function (context) {
     });
   });
 
+	var options = { 
+	        success:       showResponse,
+	        url:           "http://localhost/grabbit/yfrog/upload",
+	        beforeSubmit:  showRequest,
+	        target: "#block-views-friends-block_1"  
+	    }; 
+		$('#yfrog-form').submit(function() { 
+		        // inside event callbacks 'this' is the DOM element so we first 
+		        // wrap it in a jQuery object and then invoke ajaxSubmit 
+		        $(this).ajaxSubmit(options); 
+
+		        // !!! Important !!! 
+		        // always return false to prevent standard browser submit and page navigation 
+		        return false; 
+		    });
 };
+
+function showResponse(responseText, statusText)  { 
+  alert("noproblem"); 
+}
+function showRequest(formData, jqForm, options) { 
+    // formData is an array; here we use $.param to convert it to a string to display it 
+    // but the form plugin does this for you automatically when it submits the data 
+    var queryString = $.param(formData); 
+ 
+    // jqForm is a jQuery object encapsulating the form element.  To access the 
+    // DOM element for the form do this: 
+    // var formElement = jqForm[0]; 
+ 
+    alert('About to submit: \n\n' + queryString); 
+ 
+    // here we could return false to prevent the form from being submitted; 
+    // returning anything other than false will allow the form submit to continue 
+    return true; 
+}
