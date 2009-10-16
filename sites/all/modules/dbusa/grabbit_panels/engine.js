7476c3b14035a59996562282dcc85092c2caeb0a
Ivan <ivan@ivans-blackbook.gateway.2wire.net>
2009-10-16 17:02:51 -0500

    Testing file uploads

--- sites/all/modules/dbusa/grabbit_panels/engine.js
+++ sites/all/modules/dbusa/grabbit_panels/engine.js
@@ -39,7 +39,7 @@ Drupal.behaviors.grabbitPanelsAdjust = function (context) {
 
 	var options = { 
 	        success:       showResponse,
-	        url:           Drupal.settings.basePath+"yfrog/upload",
+	        url:           "http://yfrog.com/api/upload",
 	        beforeSubmit:  showRequest,
 	        target: "#block-views-friends-block_1"  
 	    }; 
