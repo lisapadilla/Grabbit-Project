a458fa2ae2f035b4ae7db0010ebd3f318cf5c16d
Ivan <ivan@ivans-blackbook.gateway.2wire.net>
2009-10-19 10:39:57 -0500

    YFROG support

--- sites/all/modules/dbusa/grabbit_panels/engine.js
+++ sites/all/modules/dbusa/grabbit_panels/engine.js
@@ -40,8 +40,6 @@ Drupal.behaviors.grabbitPanelsAdjust = function (context) {
 	var options = { 
 	        success:       showResponse,
 	        url:           Drupal.settings.basePath+"yfrog/upload",
-	        beforeSubmit:  showRequest,
-	        target: "#block-views-friends-block_1"  
 	    }; 
 		$('#yfrog-form').submit(function() { 
 		        // inside event callbacks 'this' is the DOM element so we first 
@@ -55,20 +53,6 @@ Drupal.behaviors.grabbitPanelsAdjust = function (context) {
 };
 
 function showResponse(responseText, statusText)  { 
-  alert("noproblem"); 
-}
-function showRequest(formData, jqForm, options) { 
-    // formData is an array; here we use $.param to convert it to a string to display it 
-    // but the form plugin does this for you automatically when it submits the data 
-    var queryString = $.param(formData); 
- 
-    // jqForm is a jQuery object encapsulating the form element.  To access the 
-    // DOM element for the form do this: 
-    // var formElement = jqForm[0]; 
- 
-    alert('About to submit: \n\n' + queryString); 
- 
-    // here we could return false to prevent the form from being submitted; 
-    // returning anything other than false will allow the form submit to continue 
-    return true; 
+  var url = responseText.attr("mediaurl");
+  alert(url); 
 }
