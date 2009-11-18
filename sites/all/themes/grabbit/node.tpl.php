14aef0dccf1dfe4d07dac4fce6b1bb38910dd2c6
Ivan <ivan@ivans-blackbook.gateway.2wire.net>
2009-11-18 12:39:48 -0600

    classes changes

--- sites/all/themes/grabbit/node.tpl.php
+++ sites/all/themes/grabbit/node.tpl.php
@@ -75,7 +75,7 @@
         
       </div>
       <?php }else{ ?>
-	    <?php if ($node->field_image_inline):?>
+	    <?php if ($node->field_image_inline[0]['value']):?>
 		  <span class="image-inline-rss"><img src="<?=$node->field_image_inline[0]['value'];?>" /></span>
 		<? endif; ?>
         <?php print $content; ?>
