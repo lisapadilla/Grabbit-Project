340665c29fcbcd92fc2a985c5153d4c0921549f9
Ivan <ivan@Ivans-BlackBook.local>
2009-12-03 10:44:02 -0600

    bronx

--- sites/all/modules/dbusa/facebook_grabbit/facebook_grabbit.js
+++ sites/all/modules/dbusa/facebook_grabbit/facebook_grabbit.js
@@ -428,7 +428,7 @@ $(document).ready(function() {
    $('.tooltip-deal').each(function(i){
 	 var contents=$('.tooltip_deal',$(this)).html();
 	 var pos = $(this).position();
-	alert($(this).parents('.stream-item-element').html());
+	alert($(this).parents('.stream-item-element').position().top);
 	
      $(this).simpletip({
 	   	fixed:true,
