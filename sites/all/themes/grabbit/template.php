8e8df1fd497353b4afc3e3b9166872abd3dd616a
Ivan <ivan@Ivans-BlackBook.local>
2009-12-18 16:48:51 -0600

    HTML / CSS

--- sites/all/themes/grabbit/template.php
+++ sites/all/themes/grabbit/template.php
@@ -114,6 +114,7 @@ function grabbit_preprocess(&$vars, $hook) {
  *   The name of the template being rendered ("page" in this case.)
  */
 function grabbit_preprocess_page(&$vars, $hook) {
+	print_r(arg(0));
 	if(arg(0)=='users' || (arg(0)=='user' && arg(1)=='me' && !arg(2))){
 		$vars['body_classes_array'][]='profile-display';
 		print_r($vars);
