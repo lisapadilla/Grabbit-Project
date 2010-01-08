91cc645e2b2c4194f19f377deebd930fc01c634f
Ivan <ivan@Ivans-BlackBook.local>
2010-01-08 12:34:15 -0600

    set

---
+++ sites/all/modules/dbusa/grabbit_realtime/real_engine.js
@@ -0,0 +1,17 @@
+Drupal.behaviors.realtime = function(){
+	
+	setInterval("execute_realtime()", 300000);
+	
+}
+
+function execute_realtime(){
+	
+		$.get(Drupal.settings.basePath+"execute/realtime",{user:"uid",pannel:"elpanel"},function(data){
+	      
+			if (data){
+				alert (data);
+			}else{
+				alert ("There was a problem connecting with the realtime");
+			}
+		}
+}
