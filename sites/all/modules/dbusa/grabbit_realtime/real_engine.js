8ee8354de18973e455c2472020df0874a7482e40
Ivan <ivan@Ivans-BlackBook.local>
2010-01-08 21:00:16 -0600

    testing rt

--- sites/all/modules/dbusa/grabbit_realtime/real_engine.js
+++ sites/all/modules/dbusa/grabbit_realtime/real_engine.js
@@ -1,6 +1,6 @@
 Drupal.behaviors.realtime = function(){
 	
-	setInterval("execute_realtime()", 15000);
+	setInterval("execute_realtime()", 25000);
 	//setInterval("execute_realtime()", 300000);
 	
 }
@@ -9,11 +9,12 @@ function execute_realtime(){
 	
 	$(".user-panel").each(function(){
 		alert($(this).attr('pid'));
-	  	$.get(Drupal.settings.basePath+"execute/realtime",{user:"uid",pannel:i},function(data){
+	/*  	$.get(Drupal.settings.basePath+"execute/realtime",{user:"uid",pannel:i},function(data){
 			if (data){
 				alert (data);
 			}
-		});	
+		});
+		*/	
 	});
 		
 }
