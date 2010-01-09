d715a47e347abe97d17af6d860715d458aeada7a
Ivan <ivan@Ivans-BlackBook.local>
2010-01-08 20:58:14 -0600

    testing rt

--- sites/all/modules/dbusa/grabbit_realtime/real_engine.js
+++ sites/all/modules/dbusa/grabbit_realtime/real_engine.js
@@ -1,13 +1,14 @@
 Drupal.behaviors.realtime = function(){
 	
-	setInterval("execute_realtime()", 8000);
+	setInterval("execute_realtime()", 15000);
 	//setInterval("execute_realtime()", 300000);
 	
 }
 
 function execute_realtime(){
 	
-	$(".user-panel").each(function(i){
+	$(".user-panel").each(function(){
+		alert($(this).attr('pid'));
 	  	$.get(Drupal.settings.basePath+"execute/realtime",{user:"uid",pannel:i},function(data){
 			if (data){
 				alert (data);
