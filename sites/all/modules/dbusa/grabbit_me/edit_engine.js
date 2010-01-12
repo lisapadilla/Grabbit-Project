80841ad9e7e3ad1f023cf9d02a930d8bdbba4a26
Ivan <ivan@Ivans-BlackBook.local>
2010-01-12 15:32:57 -0600

    CSS update

--- sites/all/modules/dbusa/grabbit_me/edit_engine.js
+++ sites/all/modules/dbusa/grabbit_me/edit_engine.js
@@ -1,9 +1,18 @@
 Drupal.behaviors.editables = function(){
 	
-	$(".bio").editable({type:"textarea"});
+	$(".other-info .bio").editable({type:"textarea"});
+	$(".other-info .city").editable({type:"textfield"});
+	$(".other-info .state").editable({type:"textfield"});
+	$(".other-info .country").editable({type:"textfield"});
+	$(".other-info .websites").editable({type:"textfield"});
 	
 	$(".edit-me-place").click(function (){
-		$(".bio").click();
+		$(".other-info .bio").click();
+		$(".other-info .city").click();
+		$(".other-info .state").click();
+		$(".other-info .country").click();
+		$(".other-info .websites").click();
+		return false;
 	});
 	
 }
