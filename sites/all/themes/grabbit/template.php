1dfb7df1b627af3dd9121b0f8eb855ee9d5ee86f
Ivan <ivan@Ivans-BlackBook.local>
2010-01-11 20:18:44 -0600

    upload updates

--- sites/all/themes/grabbit/template.php
+++ sites/all/themes/grabbit/template.php
@@ -185,7 +185,7 @@ function grabbit_preprocess_node(&$vars, $hook) {
 				case "ppt":
 				case "xls":
 				case "pdf":
-				  $output = '<span class="filetype"><img src="'.base_path().path_to_theme().'/images/logos/'.$ext.'.gif"/></span><div class="download-file-type"><span class="">Download: '.l($file->filename,$file->filepath).'</span></div>';
+				  $output = '<span class="filetype"><img src="'.base_path().path_to_theme().'/images/logos/'.$ext.'.gif"/></span><span class="download-file-type">Download: '.l($file->filename,$file->filepath).'</span>';
 				break;
 				default:
 				  $output = theme('imagecache', 'image_uploads', $file->filepath, 'Grabbit Image', '');
