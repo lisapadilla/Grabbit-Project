6541de0137eaaf72451b37f7eac776b35a207a55
Ivan <ivan@Ivans-BlackBook.local>
2010-01-12 18:13:50 -0600

    ajax edits

--- sites/all/modules/dbusa/grabbit_me/edit_engine.js
+++ sites/all/modules/dbusa/grabbit_me/edit_engine.js
@@ -19,7 +19,8 @@ Drupal.behaviors.editables = function(){
 		state=$("#state").val();
 		city=$("#city").val();
 		bio=$("#bio").val();
-		
+		alert(webs);
+		return;
 		$.get(Drupal.settings.basePath+"ajax/save/bio",{bio:bio,city:city,state:state,country:country,web:webs},function(data){
 			if (data){
 				$(".other-info .websites").html(webs);
