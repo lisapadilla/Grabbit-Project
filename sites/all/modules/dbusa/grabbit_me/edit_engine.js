a5485a8a8b20a402013f2d11b8e0f1640399d4d3
Ivan <ivan@Ivans-BlackBook.local>
2010-01-13 18:15:31 -0600

    tagging programmatically

--- sites/all/modules/dbusa/grabbit_me/edit_engine.js
+++ sites/all/modules/dbusa/grabbit_me/edit_engine.js
@@ -76,4 +76,30 @@ Drupal.behaviors.editables = function(){
 		return false;
 	});
 	
+	  
+     var btnUpload=$('#upload');  
+     var status=$('#status');  
+     new AjaxUpload(btnUpload, {  
+         action: 'ajax/upload/file',  
+         //Name of the file input box  
+         name: 'uploadfile',  
+         onSubmit: function(file, ext){  
+             if (! (ext && /^(jpg|png|jpeg|gif)$/.test(ext))){  
+                   // check for valid file extension  
+                 status.text('Only JPG, PNG or GIF files are allowed');  
+                 return false;  
+             }  
+             status.text('Uploading...');  
+         },  
+         onComplete: function(file, response){  
+             //On completion clear the status  
+             status.text('');  
+             //Add uploaded file to list  
+             if(response==="success"){  
+                 $('<li></li>').appendTo('#files').html('<img src="./uploads/'+file+'" alt="" /><br />'+file).addClass('success');  
+             } else{  
+                 $('<li></li>').appendTo('#files').text(file).addClass('error');  
+             }  
+         }  
+     });
 }
