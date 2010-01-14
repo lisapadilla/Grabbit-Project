136e6150be7da8008b78bd0e5a49f0eb3bda2fa8
Ivan <ivan@Ivans-BlackBook.local>
2010-01-14 11:03:40 -0600

    image change profile

--- sites/all/modules/dbusa/grabbit_me/edit_engine.js
+++ sites/all/modules/dbusa/grabbit_me/edit_engine.js
@@ -97,7 +97,7 @@ Drupal.behaviors.editables = function(){
              status.text('');  
              //Add uploaded file to list  
              if(response){
-	alert(response.left);            
+	alert(response);            
 //	 $('.picture img').attr('src',response);
 //	             $('.image img').attr('src',response);  
                  $("#bio-no-edit").slideToggle();
