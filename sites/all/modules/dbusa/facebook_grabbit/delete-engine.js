6f89240ef3cf49388ecaf62e2d6493ef71629644
Ivan <ivan@Ivans-BlackBook.local>
2010-01-26 10:34:19 -0600

    delete fb

--- sites/all/modules/dbusa/facebook_grabbit/delete-engine.js
+++ sites/all/modules/dbusa/facebook_grabbit/delete-engine.js
@@ -4,14 +4,16 @@ Drupal.behaviors.grabbitDeleteFacebook = function (context) {
   $('.views-field-action-link-requestee a').click(function(elem){
 	jConfirm("Do you really want to remove your facebook account on grabbit? This action cannot be undone.", 'Remove account',function(confirm){
 	if(confirm==false){
-	  return 0;	// return empty to avoit panel deletion 
+	  return 0;	// return empty 
 	}
-	$('.views-field-action-link-requestee').html('Removing... this may take several minutes, please wait...');
-	$.get(Drupal.settings.basePath+"panels/delete",{panel:panel},function(data){
+	$('.views-field-action-link-requestee').html('Removing... this may take several minutes, do not close this window. please wait...');
+	$.get(Drupal.settings.basePath+"facebook/remove",{panel:panel},function(data){
 		if (data){
 			$('.views-field-action-link-requestee').html('Your facebook account has been removed');
+			return false;
 		}else{
 			$('.views-field-action-link-requestee').html('There was a problem connecting to the server, please try again latter');
+			return false;
 		}
      });
 	
