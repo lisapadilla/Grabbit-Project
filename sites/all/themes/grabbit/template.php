defd4c54909639a084382c8223bcb7f1674d03fe
Rafael Cárdenas <rafael@rafael-MBP.local>
2010-01-29 16:47:16 -0600

    news links

--- sites/all/themes/grabbit/template.php
+++ sites/all/themes/grabbit/template.php
@@ -222,6 +222,7 @@ function grabbit_preprocess_node(&$vars, $hook) {
 	  $title ='<div class="titles"><h2>'.$vars['node']->links['feedapi_feed']['title']."</h2><h1>".$vars['node']->title."</h1></div>";
     $vars['content']= $controles.$title.'<div class="news-body">'.$vars['content']."</div>"; 	
     
+    unset($vars['submitted']);
     $vars['links'] = '&raquo; '.l( 'go to original article', $vars['node']->feedapi_node->url, array( 'absolute' => true ) );
   }
 }
