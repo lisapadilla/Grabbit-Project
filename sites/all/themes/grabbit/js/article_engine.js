78e6ae20d7cf291cb126bce05d9dab4031bac0f3
Ivan <ivan@Ivans-BlackBook.local>
2010-02-02 16:07:33 -0600

    phpmailer

--- sites/all/themes/grabbit/js/article_engine.js
+++ sites/all/themes/grabbit/js/article_engine.js
@@ -4,7 +4,7 @@ Drupal.behaviors.article = function(){
 		ev.preventDefault();
 		urlcomplete = window.location;
 		myurl = encodeURIComponent(urlcomplete);
-		$.getJSON("http://api.tr.im/api/trim_url.json?url=" + myurl, 
+		$.get("http://api.tr.im/api/trim_url.json?url=" + myurl, 
 	        function(data)
 	        { 
 				alert(data);
