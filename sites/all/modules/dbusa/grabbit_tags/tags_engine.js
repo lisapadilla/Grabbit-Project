6a0e52ea52be636a2af07ffd96656e02ea7f6182
Ivan <ivan@Ivans-BlackBook.local>
2010-02-05 15:17:25 -0600

    tags bg

--- sites/all/modules/dbusa/grabbit_tags/tags_engine.js
+++ sites/all/modules/dbusa/grabbit_tags/tags_engine.js
@@ -26,7 +26,7 @@ Drupal.behaviors.tagsEngine = function(){
 
 	$.get(Drupal.settings.basePath+"tags/profile/save",{nid:tag_node},function(data){
 		if (data){
-			$('#tags-show-'+tag_node).html('<span class="tags-success">Your tags were added to your profile successfully.</span>');
+			$('#tags-show-'+tag_node).html('<span class="tags-success">The tags were added to your profile successfully.</span>');
 		}else{
 			alert('Oops, there was a problem connecting to the server. Please try again');
 		}
