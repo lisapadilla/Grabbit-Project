7532792667722a06e5db707676dfc85f742932ae
Ivan <ivan@Ivans-BlackBook.local>
2010-02-05 15:43:08 -0600

    tags icons

--- sites/all/modules/dbusa/grabbit_tags/tags_engine.js
+++ sites/all/modules/dbusa/grabbit_tags/tags_engine.js
@@ -1,10 +1,11 @@
 Drupal.behaviors.tagsEngine = function(){
   $('.bring-tags').live('click',function(i){
 	var tag_node=$(this).attr('node');
-	$(this).addClass('selected');
+	var selector = $(this);
 	$.get(Drupal.settings.basePath+"tags/display",{nid:tag_node},function(data){
 		if (data){
 			$('#tags-show-'+tag_node).html(data);
+			selector.addClass('selected');
 		}else{
 			alert('Oops, there was a problem connecting to the server. Please try again');
 		}
