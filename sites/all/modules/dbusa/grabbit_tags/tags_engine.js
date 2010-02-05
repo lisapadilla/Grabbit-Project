2a187f578f6bd071f4256eda0ae93fae5a8959c0
Ivan <ivan@Ivans-BlackBook.local>
2010-02-05 15:41:54 -0600

    tags icons

--- sites/all/modules/dbusa/grabbit_tags/tags_engine.js
+++ sites/all/modules/dbusa/grabbit_tags/tags_engine.js
@@ -1,11 +1,10 @@
 Drupal.behaviors.tagsEngine = function(){
   $('.bring-tags').live('click',function(i){
 	var tag_node=$(this).attr('node');
-	
+	$(this).addClass('selected');
 	$.get(Drupal.settings.basePath+"tags/display",{nid:tag_node},function(data){
 		if (data){
 			$('#tags-show-'+tag_node).html(data);
-			$(i).addClass('selected');
 		}else{
 			alert('Oops, there was a problem connecting to the server. Please try again');
 		}
