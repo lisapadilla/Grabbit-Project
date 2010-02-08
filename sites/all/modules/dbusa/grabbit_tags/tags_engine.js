6f59682e847227cda744fedfbe3a5ce09c5eb80a
Ivan <ivan@Ivans-BlackBook.local>
2010-02-08 16:54:22 -0600

    tags test

--- sites/all/modules/dbusa/grabbit_tags/tags_engine.js
+++ sites/all/modules/dbusa/grabbit_tags/tags_engine.js
@@ -32,38 +32,58 @@ Drupal.behaviors.tagsEngine = function(){
     var options = $(this).parent('.all-tags');
     var messages = [];
     
-  if($('#save-tags',options).is(':checked')){
+  if($('#save-tags',options).is(':checked') && $('#add-tags',options).is(':checked')){
 	var tag_tags=($('#text-area-'+tag_node,options).val());
    	$.get(Drupal.settings.basePath+"tags/save",{nid:tag_node,tags:tag_tags}, function(data){
 	  	if (data){
-		  messages[0]='item';
-		  alert(messages[0]);
+		  	$.get(Drupal.settings.basePath+"tags/profile/save",{nid:tag_node},function(data){
+				if (data){
+				    $('#tags-show-'+tag_node, parent).html('<span class="tags-success">The tags were added to your item and profile successfully.</span>').fadeIn(function(){
+					    setTimeout(function(){
+					      $(".tags-success").fadeOut("fast");
+					    }, 2000);
+					});
+					var container = $('#tags-show-'+tag_node).parents('.twitter-message-stream, .facebook-post, .node-stream-news');
+					$('.bring-tags',container).removeClass('selected');
+				}else{
+					alert('Oops, there was a problem connecting to the server. Please try again');
+				}
+			});
  	    }else{
 			alert('Oops, there was a problem connecting to the server. Please try again');
 		}
     }); 
   }
-
-  if($('#add-tags',options).is(':checked')){
-    $.get(Drupal.settings.basePath+"tags/profile/save",{nid:tag_node},function(data){
-		if (data){
-		    messages[1]='profile';
-		}else{
+  else if($('#save-tags',options).is(':checked') && !$('#add-tags',options).is(':checked')){
+    var tag_tags=($('#text-area-'+tag_node,options).val());
+   	$.get(Drupal.settings.basePath+"tags/save",{nid:tag_node,tags:tag_tags}, function(data){
+	  	if (data){
+		  	$('#tags-show-'+tag_node, parent).html('<span class="tags-success">The tags were added to your item successfully.</span>').fadeIn(function(){
+			    setTimeout(function(){
+			      $(".tags-success").fadeOut("fast");
+			    }, 2000);
+			});
+			var container = $('#tags-show-'+tag_node).parents('.twitter-message-stream, .facebook-post, .node-stream-news');
+			$('.bring-tags',container).removeClass('selected');
+ 	    }else{
 			alert('Oops, there was a problem connecting to the server. Please try again');
 		}
-	});	  
+    });	  
+  }else if(!$('#save-tags',options).is(':checked') && $('#add-tags',options).is(':checked')){
+	$.get(Drupal.settings.basePath+"tags/profile/save",{nid:tag_node},function(data){
+		if (data){
+	      	$('#tags-show-'+tag_node, parent).html('<span class="tags-success">The tags were added to your profile successfully.</span>').fadeIn(function(){
+			    setTimeout(function(){
+			      $(".tags-success").fadeOut("fast");
+			    }, 2000);
+			});
+			var container = $('#tags-show-'+tag_node).parents('.twitter-message-stream, .facebook-post, .node-stream-news');
+			$('.bring-tags',container).removeClass('selected');
+	    }else{
+		  alert('Oops, there was a problem connecting to the server. Please try again');
+		}
+	});
   }
-alert(messages[0]);
-  if(messages.length>0){
-	    alert(messages.length);
-    	$('#tags-show-'+tag_node, parent).html('<span class="tags-success">The tags were added to your '+messages.join(' and ')+' successfully.</span>').fadeIn(function(){
-		    setTimeout(function(){
-		      $(".tags-success").fadeOut("fast");
-		    }, 2000);
-		});
-		var container = $('#tags-show-'+tag_node).parents('.twitter-message-stream, .facebook-post, .node-stream-news');
-		$('.bring-tags',container).removeClass('selected');	
-  }  
 
   });
 
