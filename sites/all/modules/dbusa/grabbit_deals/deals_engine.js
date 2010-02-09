2d9c00e634a07038a6a9423aca775858afe3dff6
Rafael Cárdenas <rafael@rafael-MBP.local>
2010-02-08 18:00:46 -0600

    deals ajax func

--- sites/all/modules/dbusa/grabbit_deals/deals_engine.js
+++ sites/all/modules/dbusa/grabbit_deals/deals_engine.js
@@ -1,7 +1,7 @@
 Drupal.behaviors.deals = function()
 {
   // calculate deals on visible stream nodes
-  //streamVisibles('.panel-grabbit', '.stream-node', calculateDeals, 1500);
+  streamVisibles('.user-panel', '.stream-node', calculateDeals, 1500);
   
   // deals box behaviors
   $('.url-deal a').live('click', function(i){
