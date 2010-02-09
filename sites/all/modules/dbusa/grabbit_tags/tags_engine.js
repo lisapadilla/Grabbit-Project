0e11d0fe7f0fbf633781dbefa89953b3d58d9b76
Ivan <ivan@Ivans-BlackBook.local>
2010-02-09 00:11:26 -0600

    loader

--- sites/all/modules/dbusa/grabbit_tags/tags_engine.js
+++ sites/all/modules/dbusa/grabbit_tags/tags_engine.js
@@ -38,11 +38,11 @@ Drupal.behaviors.tagsEngine = function(){
 	  	if (data){
 		  	$.get(Drupal.settings.basePath+"tags/profile/save",{nid:tag_node},function(data){
 				if (data){
-				    $('#tags-show-'+tag_node, parent).html('<span class="tags-success">The tags were added to your item and profile successfully.</span>').fadeIn(function(){
+				    /*$('#tags-show-'+tag_node, parent).html('<span class="tags-success">The tags were added to your item and profile successfully.</span>').fadeIn(function(){
 					    setTimeout(function(){
 					      $(".tags-success").fadeOut("fast");
 					    }, 2000);
-					});
+					});*/
 					var container = $('#tags-show-'+tag_node).parents('.twitter-message-stream, .facebook-post, .node-stream-news');
 					$('.bring-tags',container).removeClass('selected');
 				}else{
