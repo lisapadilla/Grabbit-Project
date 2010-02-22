3d2b60ae0f44beae70834920a39e728d0be303c0
Ivan <ivan@Ivans-BlackBook.local>
2010-02-22 11:12:42 -0600

    updat

--- sites/all/modules/dbusa/friend_search/engine.js
+++ sites/all/modules/dbusa/friend_search/engine.js
@@ -11,5 +11,37 @@ $(document).ready(function() {
 	       $(".twitter-container").slideToggle();
 	   });
 	
+	$(".friend-favoritess").live('click',function () {
+	  //var tag = $(this);
+	  var nid= $(this).attr("uid"); // user_id
+	  this.preventDefault();
+	  alert(nid);
+	 /* if (tag.text() == "Add to favorites"){
+	    tag.text("Adding to favorites...");
+	    $.get(Drupal.settings.basePath+"favorites/save/facebook",{type:"friend",nid:nid},function(data){
+	      //Successfully added to favorites
+	      //Testing if it works
+				if (data){
+								tag.text("Remove from favorites");
+								tag.attr("class","friend_favorite-remove");
+				}else{
+								alert ("There was a connection problem. Try later");
+				}
+	    });
+	  }
+	 /* else{ //Remove from favorites
+	  tag.text("Removing from favorites...");
+	    $.get(Drupal.settings.basePath+"favorites/remove/facebook",{type:"friend",nid:nid},function(data){
+	      //Successfully removed from favorites
+	      //Testing if it works
+				if (data){
+								tag.text("Add to favorites");		
+								tag.attr("class","friend_favorite");
+				}else{								
+								alert ("There was a connection problem. Try later");
+				}
+	    });		    
+	  }*/  		  
+	});
 	
 });
