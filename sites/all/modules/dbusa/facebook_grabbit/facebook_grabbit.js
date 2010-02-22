2dfe211f340daca295c5ed694808b6e37fca1098
Ivan <ivan@Ivans-BlackBook.local>
2010-02-22 12:55:18 -0600

    friends classes favs

--- sites/all/modules/dbusa/facebook_grabbit/facebook_grabbit.js
+++ sites/all/modules/dbusa/facebook_grabbit/facebook_grabbit.js
@@ -321,7 +321,37 @@ $(document).ready(function() {
 	  
 	});	
 	
+	$(".friend-action").live('click',function (i) {
 	
+	  var tag = $(this);
+	  var nid= $(this).attr("uid"); // user_id
+	  if (tag.text() == "Add to favorites"){
+	    tag.text("Adding to favorites...");
+	    $.get(Drupal.settings.basePath+"favorites/save/facebook",{type:"friend",nid:nid},function(data){
+	      //Successfully added to favorites
+	      //Testing if it works
+				if (data){
+								tag.text("Remove from favorites");
+								tag.attr("class","friend_favorite-remove friend-action");
+				}else{
+								alert ("There was a connection problem. Try later");
+				}
+	    });
+	  }
+	  else{ //Remove from favorites
+	  tag.text("Removing from favorites...");
+	    $.get(Drupal.settings.basePath+"favorites/remove/facebook",{type:"friend",nid:nid},function(data){
+	      //Successfully removed from favorites
+	      //Testing if it works
+				if (data){
+								tag.text("Add to favorites");		
+								tag.attr("class","friend_favorite friend-action");
+				}else{								
+								alert ("There was a connection problem. Try later");
+				}
+	    });		    
+	  }  		  
+	});
 	
 	$(".trash-facebook").live('click',function () {
 	  var id = $(this).attr("face_id"); //post_id
