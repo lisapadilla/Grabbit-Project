685fd7b59057b28a8272e370fa1a5f75d24b1d8f
Ivan <ivan@Ivans-BlackBook.local>
2010-02-22 11:15:08 -0600

    test cache

--- sites/all/modules/dbusa/friend_search/engine.js
+++ sites/all/modules/dbusa/friend_search/engine.js
@@ -11,10 +11,10 @@ $(document).ready(function() {
 	       $(".twitter-container").slideToggle();
 	   });
 	
-	$(".friend-favoritess").live('click',function () {
+	$(".friend-favoritess").live('click',function (i) {
 	  //var tag = $(this);
 	  var nid= $(this).attr("uid"); // user_id
-	  this.preventDefault();
+	  i.preventDefault();
 	  alert(nid);
 	 /* if (tag.text() == "Add to favorites"){
 	    tag.text("Adding to favorites...");
