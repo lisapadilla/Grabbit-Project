52df88c38b7899f70e6bfad56f2525c2cb5b22bf
Ivan <ivan@Ivans-BlackBook.local>
2010-02-24 17:41:49 -0600

    pagination me page

--- sites/all/modules/dbusa/grabbit_me/edit_engine.js
+++ sites/all/modules/dbusa/grabbit_me/edit_engine.js
@@ -114,12 +114,12 @@ Drupal.behaviors.editables = function(){
 	   var link = $(this);
 	   $.get(Drupal.settings.basePath+"me/pagination",{page:page,pid:pid},function(data){
 			if (data){
-				link.preppend(data);
+				link.prepend(data);
 				link.attr('page',page+1);
 			}else{
 				alert("Problem connecting with the server, please try again latter.");
 			}
 		});   
-     });s
+     });
 
 }
