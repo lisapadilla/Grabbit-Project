ca79a9bca861ec04bdb7d4369c8eaf2ac62e29f1
Ivan <ivan@Ivans-BlackBook.local>
2010-02-24 17:47:09 -0600

    pagination me page

--- sites/all/modules/dbusa/grabbit_me/edit_engine.js
+++ sites/all/modules/dbusa/grabbit_me/edit_engine.js
@@ -114,7 +114,7 @@ Drupal.behaviors.editables = function(){
 	   var link = $(this);
 	   $.get(Drupal.settings.basePath+"me/pagination",{page:page,pid:pid},function(data){
 			if (data){
-				$(data).insertBefore(link);
+				$(data).insertBefore(link.parent());
 				link.attr('page',page+1);
 			}else{
 				alert("Problem connecting with the server, please try again latter.");
