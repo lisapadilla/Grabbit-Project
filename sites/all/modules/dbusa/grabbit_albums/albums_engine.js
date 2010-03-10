bf091e2a577c87260d6fe2bb8bdf4ad3d8060290
Ivan <ivan@Ivans-BlackBook.local>
2010-03-09 20:10:09 -0600

    controls album

--- sites/all/modules/dbusa/grabbit_albums/albums_engine.js
+++ sites/all/modules/dbusa/grabbit_albums/albums_engine.js
@@ -1,6 +1,8 @@
 Drupal.behaviors.albums = function(){
   $("#scroll-album").scrollable({size:1,clickable:false});
 
+  alert(Drupal.settings.new_val);
+
   $("#album-type").change(function(e){
     var val=$(this).val();
 	top.location = val;
