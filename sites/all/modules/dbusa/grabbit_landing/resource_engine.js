d329ee703da89b47a684f31c5dd31fcdefcb010a
Rafael Cárdenas <rafael@rafael-MBP.local>
2010-03-11 12:20:22 -0600

    landing engine js

--- sites/all/modules/dbusa/grabbit_landing/resource_engine.js
+++ sites/all/modules/dbusa/grabbit_landing/resource_engine.js
@@ -2,7 +2,7 @@ Drupal.behaviors.resource = function()
 {
   var uname = Drupal.settings.user_name;
   
-  $('h1.title').html(uname + "'s Facebook");
+  $('h1.title').html(uname + "&#8217;s Facebook");
   
   $('.facebook-attachment img').each(function(){
     if ( $(this).width() > 596 )
