045a9ec25d36f8be435a5d6e4610014b31e5f54c
Ivan <ivan@Ivans-BlackBook.local>
2010-03-17 10:17:19 -0600

    landing pages

--- sites/all/themes/grabbit/template.php
+++ sites/all/themes/grabbit/template.php
@@ -224,7 +224,7 @@ function grabbit_preprocess_node(&$vars, $hook) {
 				drupal_add_js(drupal_get_path('module', 'grabbit_landing').'/resource_engine.js', $type = 'module');
 				drupal_add_js(drupal_get_path('module', 'facebook_grabbit').'/facebook_grabbit.js', $type = 'module');
 				
-				$controles='<div class="links_stream_media" id="article-news">  
+				$controles='<div class="links_stream_media tooltipwraper" id="article-news">  
 					<a href="JavaScript:void(0);" title="delete" class="trash-item" item_id="'.$vars['node']->nid.'">TR</a>
 					  <a href="JavaScript:void(0);" title="retweet" class="grabb-that">RT</a>
 					  <a href="javascript:void(0);" title="comment" class="comment-news">Post a comment</a>
