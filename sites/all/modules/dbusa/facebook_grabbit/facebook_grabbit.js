af845f6913c5f0a56ca4aab681f948a4427a8ceb
Ivan <ivan@Ivans-BlackBook.local>
2010-03-16 20:23:30 -0600

    tags

--- sites/all/modules/dbusa/facebook_grabbit/facebook_grabbit.js
+++ sites/all/modules/dbusa/facebook_grabbit/facebook_grabbit.js
@@ -277,6 +277,75 @@ $(document).ready(function() {
 				}
     });
 	});
+	
+	$(".facebook_comment_submit_landing").live('click',function () {
+		var input1=$(this).parents('.facebook-post');
+		$('.display-comments-buttons',input1).show();
+		var tag = $(this);
+		var post_id = $(this).attr("id");
+		var comment = $(this).prev("input").attr("value");
+		var nid=$(this).attr("nid");		
+		alert("post_id - "+post_id+" comment:"+comment+" nid="+nid);
+		/*$.get(Drupal.settings.basePath+"facebook/comment",{post_id:post_id,comment:comment,mode:"create",nid:nid},function(data){
+	      // Comment has succesfully added and the comment box is hidden again
+				if (data){
+								//Try grabbit/facebook/getuserinfo?params=first_name,last_name,sex
+								//To know the order facebook returns values
+								var params = "name,pic_square,profile_url";
+								
+								$.get(Drupal.settings.basePath+"facebook/getuserinfo",{params:params},function(data1){
+									var params_values = data1.split(",");
+									var name 				= params_values[0];
+									var pic_square 	= params_values[2];
+									var profile_url = params_values[3];								
+									
+									var d = new Date();
+									var curr_date 		= d.getDate();
+									var curr_day 			= d.getDay();
+									var curr_month 		= d.getMonth();
+									var curr_year 		= d.getFullYear();
+									var curr_hour 		= d.getHours();
+									var curr_minutes 	= d.getMinutes();													
+								
+									var date 		= curr_date+", "+curr_month+"/"+curr_day+"/"+curr_year+" - "+ curr_hour+":"+curr_minutes;
+									var comment = tag.prev("input").attr("value");
+									
+									var new_content 	= '<div id="'+data+'" class="facebook-comments clearfix" style="display:none"><a target="_blank" title="'+name+'" class="facebook-image-comment" href="'+profile_url+'"><span class="facebook-user-pic"><img class="facebook-image-comm" alt="'+name+'" src="'+pic_square+'" width="30"/></span></a>';
+									    new_content  += '<span class="facebook-comment-timeline"><a target="_blank" title="'+name+'" class="facebook-comment-link" href="'+profile_url+'">'+name+'</a></span>';
+											new_content  += '<span class="facebook-comment-text">'+comment+'</span><span class="facebook-date">'+date+'</span><a id="facebook-comment-delete-'+data+'" class="facebook-comment-delete" href="javascript:void(0)">delete comment</a></span></div>';
+									
+									// Add the new comment								
+									tag.closest("div").parent().prev().append(new_content);
+									var id = data;
+									
+									// Add the new handler to jQuery using live() function
+									$('#facebook-comment-delete-'+data).live('click',function(){
+										var tag = $(this).parent();
+										var post_id = id;
+										
+										$.get(Drupal.settings.basePath+"facebook/comment",{post_id:post_id,mode:"delete"},function(data){
+												// Comment has been deleted
+												if (data){
+													tag.slideUp('medium',function(){
+														tag.remove();
+													});		
+												}else{
+														// An error has occurred, catch it depending on the incoming design
+												}
+										});	
+									});
+									
+									// Reset comment textbox
+									tag.prev("input").attr("value","");
+									tag.prev("input").focus();
+									// Call a funcion to extend the stream and enable the new comment
+									tag.closest("div").parent().prev().children("#"+data).slideToggle('medium');						
+								});								
+				}else{
+								alert ("There was a connection problem. Try later");
+				}
+    });*/
+	});
 		
 	$(".facebook-comment-delete").click(function () {
 		var tag = $(this).parent().parent();
