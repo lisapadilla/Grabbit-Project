c562a59d92f8effe2f4594449f578ca1cfc3d674
Ivan <ivan@Ivans-BlackBook.local>
2010-03-18 10:04:02 -0600

    landing pages

--- sites/all/themes/grabbit/template.php
+++ sites/all/themes/grabbit/template.php
@@ -255,11 +255,23 @@ function grabbit_preprocess_node(&$vars, $hook) {
 		//controles
 		if($user->uid>0){
 
-			$controles='<div class="links_stream" id="article-news">  
-			    <a href="JavaScript:void(0);" class="trash-item" item_id="'.$vars['node']->nid.'">TR</a>
-			    <a href="JavaScript:void(0);" class="grabb-that">RT</a>
-			    <a href="javascript:void(0)" class="comment-news">Comment</a>
-			    <span class="flag-wrapper flag-bookmarks">'.flag_create_link('bookmarks', $vars['node']->nid).'	</span>
+			drupal_add_js($variables,'setting');
+			drupal_add_js(drupal_get_path('module', 'grabbit_landing').'/resource_engine.js', $type = 'module');
+			drupal_add_js(drupal_get_path('module', 'facebook_grabbit').'/facebook_grabbit.js', $type = 'module');
+			if($vars['node']->field_networks[0]['value']){
+				$networks=unserialize($vars['node']->field_networks[0]['value']);
+				$network_id=array_shift($networks);
+			}else{
+				$network_id=$vars['node']->nid;
+			}
+			$controles='<div class="links_stream_media tooltipwraper" id="article-news">
+			    <div id="tags-show-'.$network_id.'"></div>  
+				<a href="JavaScript:void(0);" title="delete" class="trash-item" item_id="'.$vars['node']->nid.'">TR</a>
+				  <a href="JavaScript:void(0);" title="retweet" class="grabb-that">RT</a>
+				  <a href="javascript:void(0);" title="comment" class="comment-news">Post a comment</a>
+				  <a href="javascript:void(0);" title="add/edit tags" class="bring-tags iconchange" node="'.$network_id.'"></a>
+				   <span class="url-deal iconchange"><a href="#" title="check deals" node="'.$network_id.'" >Deal</a></span>
+			      <span class="flag-wrapper flag-bookmarks">'.flag_create_link('bookmarks', $vars['node']->nid).'	</span>
 			  </div>';
 			
 		}
