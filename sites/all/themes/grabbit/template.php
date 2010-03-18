43c46c30eb5b4c3f60f5193a8d629f8329b47ad9
Ivan <ivan@Ivans-BlackBook.local>
2010-03-18 17:09:42 -0600

    landing pages

--- sites/all/themes/grabbit/template.php
+++ sites/all/themes/grabbit/template.php
@@ -182,8 +182,8 @@ function grabbit_preprocess_node(&$vars, $hook) {
 			}else{
 				$network_id=$vars['node']->nid;
 			}
-			$controles='<div class="links_stream_media" id="article-news">
-			    <div id="tags-show-'.$network_id.'"></div>  
+			$controles='<div id="tags-show-'.$network_id.'"></div>  
+			<div class="links_stream_media" id="article-news">
 				<a href="JavaScript:void(0);" title="delete" class="trash-item" item_id="'.$vars['node']->nid.'">TR</a>
 				  <a href="JavaScript:void(0);" title="retweet" class="grabb-that">RT</a>
 				  <a href="javascript:void(0);" title="comment" class="comment-news comment-special">Post a comment</a>
