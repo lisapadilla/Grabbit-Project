7c2bcc4b4cb6efb36a668373f570bc989a221c54
Ivan <ivan@evdomip-49-174.iusacell.net>
2010-03-31 09:40:53 -0600

    btostream

--- sites/all/themes/grabbit/node.tpl.php
+++ sites/all/themes/grabbit/node.tpl.php
@@ -50,7 +50,7 @@
 ?>
 <div<?php print $attributes; ?> class="node-<?=$node->type?>">
     <div class="meta">
-	
+	<div class="backto"><a href="#" class="backtostream">Back to stream</a></div>
 		<?php if ($links_stream): ?>
 	      <div class="links_stream">
 	        <?php print $comment_link; ?>
