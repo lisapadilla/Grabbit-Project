1d34e0c1e943f4578a9cf778cdf886df3e2abcc3
Ivan <ivan@Ivans-BlackBook.local>
2010-04-05 19:58:21 -0500

    ipad scroll

--- sites/all/modules/dbusa/facebook_grabbit/facebook_grabbit.js
+++ sites/all/modules/dbusa/facebook_grabbit/facebook_grabbit.js
@@ -108,7 +108,48 @@ function bringPanelsAjax(currentSlide,NextSlide){
 		}
 	});
 }
+
+//**************************
+// Detects if the current device is an iPhone.
+function DetectIphone()
+{
+   if (uagent.search(deviceIphone) > -1)
+      return true;
+   else
+      return false;
+}
+
+//**************************
+// Detects if the current device is an iPod Touch.
+function DetectIpod()
+{
+   if (uagent.search(deviceIpod) > -1)
+      return true;
+   else
+      return false;
+}
+
+//**************************
+// Detects if the current device is an iPhone or iPod Touch.
+function DetectIphoneOrIpod()
+{
+    if (DetectIphone())
+       return true;
+    else if (DetectIpod())
+       return true;
+    else
+       return false;
+}
+
 $(document).ready(function() {
+
+  	var deviceIphone = "iphone";
+	var deviceIpod = "ipod";
+
+	//Initialize our user agent string to lower case.
+	var uagent = navigator.userAgent.toLowerCase();
+	
+	alert(uagent);
 
 
    $(".suser-panels .user-panel").each(function(elemento,n){
