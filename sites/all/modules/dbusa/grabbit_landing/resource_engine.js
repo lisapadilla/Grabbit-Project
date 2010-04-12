54a073c2bf0934b85e79f7b379c932cf7f382f1d
Ivan <ivan@Ivans-BlackBook.local>
2010-04-12 18:37:29 -0500

    facebook vs twitter

--- sites/all/modules/dbusa/grabbit_landing/resource_engine.js
+++ sites/all/modules/dbusa/grabbit_landing/resource_engine.js
@@ -1,8 +1,9 @@
 Drupal.behaviors.resource = function()
 {
   var uname = Drupal.settings.user_name;
+  var type  = Drupal.settings.user_type;
   if(uname){
-	$('h1.title').html(uname + "&#8217;s Facebook");	
+	$('h1.title').html(uname + "&#8217;s "+type);	
   }
   
   $('.facebook-attachment img').each(function(){
