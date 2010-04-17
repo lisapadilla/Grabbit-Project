4581eb071b214633e90d40eb36e54f343dfc065f
Ivan <ivan@Ivans-BlackBook.local>
2010-04-17 09:22:01 -0500

    bug taxonomy

--- sites/all/themes/grabbit/js/engine.js
+++ sites/all/themes/grabbit/js/engine.js
@@ -2,4 +2,6 @@ Drupal.behaviors.tooltips = function()
 {
   $('#article-news').addClass('tooltipwraper');
   $(".tooltipwraper a[title]").tooltip('#tooltip'); 
+
+  $("#edit-taxonomy-tags-3").removeClass('form-autocomplete');
 }
