f5d9a184af2da2a1de89cb5a7abfd9a6fdafb06f
Ivan <ivan@Ivans-BlackBook.local>
2010-04-17 19:23:04 -0500

    container

--- sites/all/themes/grabbit/template.php
+++ sites/all/themes/grabbit/template.php
@@ -392,10 +392,7 @@ function curPageURL() {
  $pageURL = 'http';
  if ($_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
  $pageURL .= "://";
- if ($_SERVER["SERVER_PORT"] != "80") {
-  $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
- } else {
-  $pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
- }
+ $pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+ 
  return $pageURL;
 }
