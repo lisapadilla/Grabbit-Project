f111a22e10b71328a9f3bd0e5053cb566e74ac22
Ivan <ivan@Ivans-BlackBook.local>
2010-05-27 14:28:33 -0500

    search updates

--- sites/all/modules/dbusa/grabbit_search/search_engine.js
+++ sites/all/modules/dbusa/grabbit_search/search_engine.js
@@ -1,7 +1,7 @@
 Drupal.behaviors.grabbitSearchEngine = function (context) {
   var count=0;
   var basic=0;
-  $('.panel-favorites div').each(function(i){
+  $('.stream-node').each(function(i){
 	count++;
 	if(count<=10){
 		basic++;
@@ -11,5 +11,9 @@ Drupal.behaviors.grabbitSearchEngine = function (context) {
   });
 
   $('a.grabbit-results').html('Results: '+basic+' of '+count);
+
+  if(count > basic){
+	$('.suser-panels').append('<span style="position: absolute;"><a href="#">MORE</a></span>');
+}
 
 };
