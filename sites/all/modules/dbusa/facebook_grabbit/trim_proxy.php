b0893bd580400c1795d464cf24fb5ce7c981a4ed
Ivan <ivan@Ivans-BlackBook.local>
2010-07-23 12:09:00 -0500

    url

--- sites/all/modules/dbusa/facebook_grabbit/trim_proxy.php
+++ sites/all/modules/dbusa/facebook_grabbit/trim_proxy.php
@@ -1,5 +1,5 @@
 <?php 
 header('Content-type: application/json');
 $dest=$_GET['url'];
-echo file_get_contents('http://api.bit.ly/v3/shorten?login=grabbit&apiKey=R_3ec7e2bdc7065f1a5847789192659d72format=json&longUrl='.$dest);
+echo file_get_contents('http://api.bit.ly/v3/shorten?login=grabbit&apiKey=R_3ec7e2bdc7065f1a5847789192659d72&format=json&longUrl='.$dest);
 ?>
